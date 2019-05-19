02ef7914c2c8550068d3cfa7c5cef4628ce25bac
kiran <salte.kiran@gmail.com>
2019-05-19 14:46:04 -0700

    My first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\salte\PycharmProjects\Simple_social_project\simplesocial\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
